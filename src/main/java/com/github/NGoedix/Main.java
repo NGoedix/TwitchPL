8e3bc165b29b7d99b666b277a23e0985d7d8c2a5
NGoedix <goedix@wearehackerone.com>
2024-04-14 22:31:15 +0200

    Fix: Some fixes

--- src/main/java/com/github/NGoedix/Main.java
+++ src/main/java/com/github/NGoedix/Main.java
@@ -1,22 +1,26 @@
 package com.github.NGoedix;
 
 import com.github.NGoedix.twitchpl.StreamNotFound;
-import com.github.NGoedix.twitchpl.StreamQuality;
-import com.github.NGoedix.twitchpl.TwitchUtil;
+import com.github.NGoedix.test.StreamQuality;
+import com.github.NGoedix.test.TwitchUtil;
 
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String stream = "onepiecedirecto";
+        String stream = "illojuan";
+
 
         try {
-            List<StreamQuality> qualities = TwitchUtil.getStream(stream);
+            List<StreamQuality> qualities = TwitchUtil.getVod("2116939083");
+//            List<StreamQuality> qualities = TwitchUtil.getStream(stream);
 
             for (StreamQuality quality : qualities)
                 System.out.println(quality.getUrl());
-        } catch (StreamNotFound e) {
+
+            System.out.println(qualities);
+        } catch (TwitchUtil.StreamNotFound e) {
             System.out.println(e.getMessage());
         }
     }
